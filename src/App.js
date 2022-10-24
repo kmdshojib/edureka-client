9a00051233b00718fbb52b1ee9d05c41a5419464
kmdshojib <kmd.shojib123@gmail.com>
2022-10-24 22:28:58 +0600

    basic router setup compleate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// components 
+import Main from './layout/main';
+import LogIn from './components/login/login';
+import Register from './components/register/register';
 
 function App() {
+
+  const route = createBrowserRouter([
+    {
+      path:"/",
+      element: <Main />,
+      children:[
+        {
+          path:"/login",
+          element: <LogIn />
+        },
+        {
+          path:"/register",
+          element: <Register />
+        }
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      
+        <RouterProvider router={route}/>
     </div>
   );
 }
---
+++ src/components/Header/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar,Container,Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <Navbar bg="light" variant="light">
+        <Container>
+        <Navbar.Brand><NavLink>Edureka</NavLink></Navbar.Brand>
+        <Nav className="me-auto">
+            <NavLink className="nav-link" to="/login">Login</NavLink>
+            <NavLink className="nav-link" to="/register">Register</NavLink>
+        </Nav>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
