560b9d72952de68129b010b2d31fffe0ad76f52e
kmdshojib <kmd.shojib123@gmail.com>
2022-10-24 22:00:17 +0600

    project initiated

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdJHUq47VzWfCYFbDOpyjJAtoy_shWTvs",
+  authDomain: "educify-as-10.firebaseapp.com",
+  projectId: "educify-as-10",
+  storageBucket: "educify-as-10.appspot.com",
+  messagingSenderId: "789651450826",
+  appId: "1:789651450826:web:a87ece442cea7db8b0fd33"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
