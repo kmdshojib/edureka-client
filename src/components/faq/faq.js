0b81d234747adc60edb09afa07d5927cac4ad730
kmdshojib <kmd.shojib123@gmail.com>
2022-10-27 02:19:26 +0600

    faq added

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Accordion } from 'react-bootstrap';
 
 const FAQ = () => {
   return (
-    <div>FAQ</div>
+    <div className='container mt-5 w-75'>
+      <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Will you suppoort when course ends?</Accordion.Header>
+        <Accordion.Body>
+          yes, we will surely be able to support you will finish the course!!
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>What this course contains?</Accordion.Header>
+        <Accordion.Body>
+          This course contains the following curriculum will be given to you.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
   )
 }
 
